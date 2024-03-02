b7366f32f87837704641bfa19e104e606def73de
Brandon Durand <ptco.dev@gmail.com>
2024-03-02 17:21:52 -0600

    Complete

--- js/script.js
+++ js/script.js
@@ -8,20 +8,32 @@ For assistance:
    Reach out in your Slack community if you have questions
 */
 
+
+const header = document.querySelector('header');
+const student_list = document.querySelector('.student-list');
+let numOfResults = data.length;
+
+
+// Function checks number of students passed in not empty and updates results number
+function showResultsNum(list){
+
+   if(list[0] !== ''){
+      document.querySelector('.ResultsNum > b').textContent = list.length.toString();
+   }
+   else {
+      document.querySelector('.ResultsNum > b').textContent = 0;
+   }
+
+}
+
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
-
 function showPage(list, page){
 
    const startIndex = (page * 9) - 9
-
    const endIndex = (page * 9)
-
-   const student_list = document.querySelector('.student-list');
-
-
    student_list.innerHTML = '';
 
    list.map((student, index)=>{
@@ -31,7 +43,7 @@ function showPage(list, page){
             <li class="student-item cf">
                <div class="student-details">
                   <img class="avatar" src="${student.picture.large}" alt="Profile Picture">
-                  <h3>${student.name.title}${student.name.first}${student.name.last}</h3>
+                  <h3>${student.name.title} ${student.name.first} ${student.name.last}</h3>
                   <span class="email">${student.email}</span>
                </div>
                <div class="joined-details">
@@ -43,8 +55,6 @@ function showPage(list, page){
       }
    })
 
-
-
 }
 
 /*
@@ -52,6 +62,8 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 function addPagination(list){
+
+   showResultsNum(list)
 
    const paginationBtns = Math.ceil(list.length / 9);
 
@@ -68,10 +80,12 @@ function addPagination(list){
 
    }
 
+   // Assigns active class to first pagination button on page load
    link_list.firstElementChild.firstElementChild.className = 'active'
 
    link_list.addEventListener('click', e => {
       
+      // Checks if Search Button is clicked
       if(e.target.tagName === 'BUTTON'){
 
          link_list.firstElementChild.firstElementChild.className = ''
@@ -101,35 +115,60 @@ function addSearchBar(){
       </label>
    `;
 
-   document.querySelector('header').insertAdjacentHTML('beforeend', searchBar) 
+   header.insertAdjacentHTML('beforeend', searchBar) 
 
-   
-   document.querySelector('.student-search').addEventListener('click', e=>{
-
+   // Function filters and returns students that match the user's seach query
+   function Search(e){
       const searchInput = document.querySelector('#search').value;
 
-      const regex = /([^\d]+)/i;
+      const regex = /([^\d\s]+)/i;
 
-      if(e.target.tagName === 'BUTTON'){
+      let Results = [];
+
+      if(e.target.tagName === 'BUTTON' || e.target.tagName === 'INPUT'){
+
+         Results = [];
 
          data.map((student)=>{
 
-            if(regex.test(searchInput)){
-               console.log(student.name.first.replace(regex, '$1'));
+            if(regex.test(searchInput) && student.name.first.toLowerCase().includes(searchInput.toLowerCase())){
+               Results.push(student);
             }
-            
-
          })
 
-         console.log(data[0].name.first.replace(regex, '$1'))
-      }
+         if(Results.length !== 0){
+            showPage(Results, 1);
+            addPagination(Results);  
+         }
+         else {
+            const resultMsg = `
+               <h3 class="ResultMsg"> No Results Found </h3>
+            `;
+            student_list.innerHTML = resultMsg;
+            addPagination([''])
+         }
 
+      }
+   }
+   
+   // Search for student on search button click
+   document.querySelector('.student-search').addEventListener('click', e=>{
+      Search(e)
    })
+
+   // Search for student on user input
+   document.querySelector('.student-search').addEventListener('keyup', e=>{
+      Search(e);
+   })
+
 }
 
+const numResultMsg = `<h3 class="ResultsNum">Results <b>${numOfResults}</b></h3>`;
+header.insertAdjacentHTML('beforeend', numResultMsg);
 
 // Call functions
+addSearchBar();
 showPage(data, 1);
 addPagination(data);
-addSearchBar();
+
 
